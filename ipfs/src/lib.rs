3e4d0843034142249726b89dbfce05b148b11177
Byron Guina <byron.guina@gmail.com>
2025-05-05 09:58:40 -0700

    feat: read from IPFS

--- ipfs/src/lib.rs
+++ ipfs/src/lib.rs
@@ -1,5 +1,55 @@
+use grc20::pb::ipfs::Edit;
 use prost::Message;
+use reqwest::Client as ReqwestClient;
 
-pub fn deserialize<T: Message + Default>(buf: &[u8]) -> Result<T, prost::DecodeError> {
-    T::decode(buf)
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("prost error: {0}")]
+    Prost(#[from] prost::DecodeError),
+    #[error("cid error: {0}")]
+    CidError(String),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+pub fn deserialize(buf: &[u8]) -> std::result::Result<Edit, prost::DecodeError> {
+    Edit::decode(buf)
+}
+
+pub struct IpfsClient {
+    url: String,
+    client: ReqwestClient,
+}
+
+impl IpfsClient {
+    pub fn new(url: &str) -> Self {
+        IpfsClient {
+            url: url.to_string(),
+            client: ReqwestClient::new(),
+        }
+    }
+
+    pub async fn get(&self, hash: &str) -> Result<Edit> {
+        // @TODO: Error handle
+        let cid = if let Some((_, maybe_cid)) = hash.split_once("://") {
+            maybe_cid
+        } else {
+            ""
+        };
+
+        let bytes = self.get_bytes(cid).await?;
+        let data = deserialize(&bytes)?;
+        return Ok(data);
+    }
+
+    pub async fn get_bytes(&self, hash: &str) -> Result<Vec<u8>> {
+        let url = format!("{}{}", self.url, hash);
+        let res = self.client.get(&url).send().await?;
+        let bytes = res.bytes().await?;
+        Ok(bytes.to_vec())
+    }
 }
