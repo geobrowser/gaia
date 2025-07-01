6d07619f747fc0bbf89ef745a9f1c5d55e9c5880
Byron Guina <byron.guina@gmail.com>
2025-07-01 14:07:12 -0700

    feat: latest migration

--- deployer/index.ts
+++ deployer/index.ts
@@ -1,34 +1,80 @@
 import {Graph, type Op} from "@graphprotocol/grc-20"
-import rootData from "./25omwWh6HYgeRQKCaSpVpa_ops.json" // 2258 ops
-import cryptoData from "./SgjATMbm41LX6naizMqBVd_ops.json" // 22788 ops
-import cryptoEventsData from './LHDnAidYUSBJuvq7wDPRQZ_ops.json' // 3701 ops
-import regionsData from './D8akqNQr8RMdCdFHecT2n_ops.json'
+import rootData from "./root.json" // 2258 ops
+import cryptoData from "./crypto.json" // 22788 ops
+import cryptoEventsData from './crypto-events.json' // 3701 ops
+import regionsData from './regions.json'
+import cryptoNewsData from './crypto-news.json';
+import sfData from './sf.json'
+import industriesData from './industries.json'
+import educationData from './education.json'
+import academiaData from './academia.json'
+import technologyData from './technology.json'
 
 const ROOT_ENTITY_ID = "6b9f649e-38b6-4224-927d-d66171343730"
 const CRYPTO_ENTITY_ID = "23575692-bda8-4a71-8694-04da2e2af18f"
 const CRYPTO_EVENTS_ENTITY_ID = "320ab568-68cf-4587-8dc9-ae82f55587ce"
 const REGIONS_ENTITY_ID = "1d7ee87f-70d7-462d-9b72-ce845aa15986"
+const CRYPTO_NEWS_ENTITY_ID = "fd34c360-59ca-4284-9f13-44d81da56837"
+const SF_ENTITY_ID = "16faead7-86d6-4579-b7ea-e43cbdb2db05"
+const INDUSTRIES_ENTITY_ID = "51725d6a-21b2-4396-a89c-1b7d2008ac65"
+const EDUCATION_ENTITY_ID = "be259c52-532d-4269-9a6c-e93dd4f11e17"
+const ACADEMIA_ENTITY_ID = "0fa96f99-1faa-48f2-b825-a1113de0e4be"
+const TECHNOLOGY_ENTITY_ID = "a56e7d86-9a0a-47df-a1f9-6c2cf658f79e"
 
-const deployables = [{
-  entityId: ROOT_ENTITY_ID,
-  name: "Geo",
-  data: rootData.data,
-}, {
-  entityId: CRYPTO_ENTITY_ID,
-  name: "Crypto",
-  data: cryptoData.data,
-}, {
-  entityId: CRYPTO_EVENTS_ENTITY_ID,
-  name: "Crypto events",
-  data: cryptoEventsData.data,
-}, {
-  entityId: REGIONS_ENTITY_ID,
-  name: "Regions",
-  data: regionsData.data
-}]
+const deployables = [
+//   {
+//   entityId: ROOT_ENTITY_ID,
+//   name: "Geo",
+//   data: rootData,
+// },
+// {
+//   entityId: CRYPTO_ENTITY_ID,
+//   name: "Crypto",
+//   data: cryptoData,
+// },
+// {
+//   entityId: CRYPTO_EVENTS_ENTITY_ID,
+//   name: "Crypto events",
+//   data: cryptoEventsData,
+// }, {
+//   entityId: REGIONS_ENTITY_ID,
+//   name: "Regions",
+//   data: regionsData
+// },
+// {
+//   entityId: CRYPTO_NEWS_ENTITY_ID,
+//   name: "Crypto news",
+//   data: cryptoNewsData
+// },
+{
+  entityId: SF_ENTITY_ID,
+  name: "San Francisco",
+  data: sfData
+},
+  // {
+  //   entityId: INDUSTRIES_ENTITY_ID,
+  //   name: "Industries",
+  //   data: industriesData
+  // },
+  // {
+  //   entityId: EDUCATION_ENTITY_ID,
+  //   name: "Education",
+  //   data: educationData
+  // },
+  // {
+  //   entityId: ACADEMIA_ENTITY_ID,
+  //   name: "Academia",
+  //   data: academiaData
+  // },
+  // {
+  //   entityId: TECHNOLOGY_ENTITY_ID,
+  //   name: "Technology",
+  //   data: technologyData
+  // }
+]
 
 for (const deploy of deployables) {
-  console.log(`Deploying ${deploy.name} with ${regionsData.data.length} ops`)
+  console.log(`Deploying ${deploy.name} with ${deploy.data.length} ops`)
 
   const space = await Graph.createSpace({
   	editorAddress: "0x84713663033dC5ba5699280728545df11e76BCC1",
@@ -41,8 +87,14 @@ for (const deploy of deployables) {
   console.log("space", space)
 }
 
-// 55053
-// Root 64ed9ffa-e7b3-40f6-ae99-fbf6112d10f8
-// Crypto 065d5e6f-0b3c-45b3-a5db-ace191e5a35c
-// Crypto news d4cd9afa-3edf-4739-8537-ebb46da159f7
-// Regions 1f230cb3-145c-4e4b-b325-e85b0f8a212e
+// 56013
+// Root 2df11968-9d1c-489f-91b7-bdc88b472161
+// Crypto b2565802-3118-47be-91f2-e59170735bac
+// Crypto news dabe3133-4334-47a0-85c5-f965a3a94d4c
+// Regions aea9f05a-2797-4e7e-aeae-5059ada3b56b
+// Crypto news 27af9116-ddb6-4baa-b4f0-c54f0774d346
+// Industries bf44e948-07e0-4297-a2e7-371c22670f98
+// Education 0fcbc499-71e5-4505-a081-aa26edd97937
+// Academia b9192469-f28f-498c-a486-b78f68ab05f0
+// Technology d1a3b7e7-37c0-4126-b2bf-53ff46977fb2
+// SF b42fa1af-1d67-4058-a6f1-4be5d7360caf
--- indexer/tests/migration.rs
+++ indexer/tests/migration.rs
@@ -45,39 +45,48 @@ async fn main() -> Result<(), IndexingError> {
 
     let root_space_bytes = fs::read("./tests/25omwWh6HYgeRQKCaSpVpa_ops");
     let crypto_space_bytes = fs::read("./tests/SgjATMbm41LX6naizMqBVd_ops");
-    let crypto_news_space_bytes = fs::read("./tests/LHDnAidYUSBJuvq7wDPRQZ_ops");
+    let crypto_events_space_bytes = fs::read("./tests/LHDnAidYUSBJuvq7wDPRQZ_ops");
     let regions_space_bytes = fs::read("./tests/D8akqNQr8RMdCdFHecT2n_ops");
+    let crypto_news_space_bytes = fs::read("./tests/BDuZwkjCg3nPWMDshoYtpS_ops");
 
     let root_space_edit = Edit::decode(Bytes::from(root_space_bytes.unwrap()));
     let crypto_space_edit = Edit::decode(Bytes::from(crypto_space_bytes.unwrap()));
-    let crypto_news_space_edit = Edit::decode(Bytes::from(crypto_news_space_bytes.unwrap()));
+    let crypto_events_space_edit = Edit::decode(Bytes::from(crypto_events_space_bytes.unwrap()));
     let regions_space_edit = Edit::decode(Bytes::from(regions_space_bytes.unwrap()));
+    let crypto_news_space_edit = Edit::decode(Bytes::from(crypto_news_space_bytes.unwrap()));
+
+    println!(
+        "crypto news ops length {}",
+        crypto_events_space_edit.clone().unwrap().ops.len()
+    );
 
     let root_space_preprocessed_edit = PreprocessedEdit {
-        // For now we use a random UUID instead of the correct UUID for the root space
         space_id: Uuid::parse_str("8ef40bdd-cf69-4ad7-a9a1-f71c15653994").unwrap(),
         edit: Some(root_space_edit.clone().unwrap()),
         is_errored: false,
     };
 
     let crypto_space_preprocessed_edit = PreprocessedEdit {
-        // For now we use a random UUID instead of the correct UUID for the crypto space
         space_id: Uuid::parse_str("aa84b08d-779a-495c-93f1-44e667baf6d7").unwrap(),
         edit: Some(crypto_space_edit.clone().unwrap()),
         is_errored: false,
     };
 
-    let crypto_news_space_preprocessed_edit = PreprocessedEdit {
-        // For now we use a random UUID instead of the correct UUID for the crypto space
+    let crypto_events_space_preprocessed_edit = PreprocessedEdit {
         space_id: Uuid::parse_str("26c7edb1-ca79-4a56-8cc6-04a8e689a8f0").unwrap(),
-        edit: Some(crypto_news_space_edit.clone().unwrap()),
+        edit: Some(crypto_events_space_edit.clone().unwrap()),
         is_errored: false,
     };
 
     let regions_space_preprocessed_edit = PreprocessedEdit {
-        // For now we use a random UUID instead of the correct UUID for the crypto space
         space_id: Uuid::parse_str("c0814872-672e-46c0-8cdb-73ad34e0b77b").unwrap(),
         edit: Some(regions_space_edit.clone().unwrap()),
+        is_errored: false,
+    };
+
+    let crypto_news_preprocessed_edit = PreprocessedEdit {
+        space_id: Uuid::parse_str("41f4a7ce-159d-4bfc-beca-753ede566d91").unwrap(),
+        edit: Some(crypto_news_space_edit.clone().unwrap()),
         is_errored: false,
     };
 
@@ -106,7 +115,7 @@ async fn main() -> Result<(), IndexingError> {
         personal_plugin: "0x2222222222222222222222222222222222222222".to_string(),
     });
 
-    let crypto_news_space = CreatedSpace::Personal(PersonalSpace {
+    let crypto_events_space = CreatedSpace::Personal(PersonalSpace {
         dao_address: "0x098765432109876543210987654321098765432f".to_string(),
         space_address: "0xFEDCBA0987654321098765432109876543210987".to_string(),
         personal_plugin: "0x2222222222222222222222222222222222222222".to_string(),
@@ -118,22 +127,20 @@ async fn main() -> Result<(), IndexingError> {
         personal_plugin: "0x2222222222222222222222222222222222222222".to_string(),
     });
 
+    let crypto_news_space = CreatedSpace::Personal(PersonalSpace {
+        dao_address: "0x098765432109876543210987654321098765432g".to_string(),
+        space_address: "0xFEDCBA0987654321098765432109876543210987".to_string(),
+        personal_plugin: "0x2222222222222222222222222222222222222222".to_string(),
+    });
+
     let indexer = TestIndexer::new(storage.clone(), properties_cache.clone());
 
     indexer
         .run(&vec![
             KgData {
                 block: block_1,
-                edits: vec![
-                    root_space_preprocessed_edit,
-                    crypto_space_preprocessed_edit,
-                    // crypto_news_space_preprocessed_edit,
-                ],
-                spaces: vec![
-                    root_space,
-                    crypto_space,
-                    // crypto_news_space,
-                ],
+                edits: vec![root_space_preprocessed_edit, crypto_space_preprocessed_edit],
+                spaces: vec![root_space, crypto_space],
 
                 added_editors: vec![],
                 added_members: vec![],
@@ -143,17 +150,11 @@ async fn main() -> Result<(), IndexingError> {
             KgData {
                 block: block_2,
                 edits: vec![
-                    // root_space_preprocessed_edit,
-                    // crypto_space_preprocessed_edit,
-                    crypto_news_space_preprocessed_edit,
+                    crypto_events_space_preprocessed_edit,
                     regions_space_preprocessed_edit,
+                    crypto_news_preprocessed_edit,
                 ],
-                spaces: vec![
-                    // root_space,
-                    // crypto_space,
-                    crypto_news_space,
-                    regions_space,
-                ],
+                spaces: vec![crypto_events_space, regions_space, crypto_news_space],
                 added_editors: vec![],
                 added_members: vec![],
                 removed_editors: vec![],
