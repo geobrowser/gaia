eb216f7be98c0abddb61e0d56b9358dd691c1ec5
Byron Guina <byron.guina@gmail.com>
2025-07-01 23:58:06 -0700

    fns

--- api/drizzle/0002_brief_thanos.sql
+++ api/drizzle/0002_brief_thanos.sql
@@ -31,3 +31,40 @@ ALTER TABLE "values" DISABLE TRIGGER ALL;
 ALTER TABLE "relations" DISABLE TRIGGER ALL;
 ALTER TABLE "members" DISABLE TRIGGER ALL;
 ALTER TABLE "editors" DISABLE TRIGGER ALL;
+
+-- Custom SQL migration file, put your code below! --
+CREATE OR REPLACE FUNCTION public.entities_name(entity entities) RETURNS text AS $$
+  SELECT value FROM values WHERE entity_id = entity.id AND property_id = 'a126ca53-0c8e-48d5-b888-82c734c38935' LIMIT 1;
+$$ LANGUAGE sql STABLE;
+
+
+CREATE OR REPLACE FUNCTION public.entities_description(entity entities) RETURNS text AS $$
+  SELECT value FROM values WHERE entity_id = entity.id AND property_id = '9b1f76ff-9711-404c-861e-59dc3fa7d037' LIMIT 1;
+$$ LANGUAGE sql STABLE;
+
+
+CREATE OR REPLACE FUNCTION public.entities_types(entity entities) RETURNS SETOF public.entities AS $$
+  SELECT e.*
+  FROM entities e
+  INNER JOIN relations r ON e.id = r.to_entity_id
+  WHERE r.from_entity_id = entity.id AND r.type_id = '8f151ba4-de20-4e3c-9cb4-99ddf96f48f1';
+$$ LANGUAGE sql STABLE;
+
+
+CREATE OR REPLACE FUNCTION public.entities_values(entity entities) RETURNS SETOF public.values AS $$
+  SELECT v.*
+  FROM values v
+  WHERE v.entity_id = entity.id
+$$ LANGUAGE sql STABLE;
+
+CREATE OR REPLACE FUNCTION public.entities_relations(entity entities) RETURNS SETOF public.relations AS $$
+  SELECT r.*
+  FROM relations r
+  WHERE r.from_entity_id = entity.id
+$$ LANGUAGE sql STABLE;
+
+CREATE OR REPLACE FUNCTION public.entities_backlinks(entity entities) RETURNS SETOF public.relations AS $$
+  SELECT r.*
+  FROM relations r
+  WHERE r.to_entity_id = entity.id
+$$ LANGUAGE sql STABLE;
