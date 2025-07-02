eea753db2a36a1b477058aa6679e76630e0ce17b
Byron Guina <byron.guina@gmail.com>
2025-07-02 10:21:12 -0700

    fix: linting

--- api/src/services/storage/reset.ts
+++ api/src/services/storage/reset.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import {Effect} from "effect"
 
 import {
 	editors,
@@ -10,36 +10,34 @@ import {
 	relations,
 	spaces,
 	values,
-} from "../../services/storage/schema";
-import { make, Storage } from "../../services/storage/storage";
-import { Environment, make as makeEnvironment } from "../environment";
+} from "../../services/storage/schema"
+import {make, Storage} from "../../services/storage/storage"
+import {Environment, make as makeEnvironment} from "../environment"
 
 const reset = Effect.gen(function* () {
-	const db = yield* Storage;
+	const db = yield* Storage
 
 	// Run all deletes in a single transaction
 	const results = yield* db.use(async (client) => {
 		const result = await client.transaction(async (tx) => {
 			// Delete in an order that respects foreign key constraints
 			// await tx.delete(ipfsCache).execute();
-			const v = await tx.delete(values).execute();
-			const r = await tx.delete(relations).execute();
-			const ed = await tx.delete(editors).execute();
-			const m = await tx.delete(members).execute();
-			const p = await tx.delete(properties).execute();
-			const e = await tx.delete(entities).execute();
-			const s = await tx.delete(spaces).execute();
-			const c = await tx.delete(meta).execute();
+			const v = await tx.delete(values).execute()
+			const r = await tx.delete(relations).execute()
+			const ed = await tx.delete(editors).execute()
+			const m = await tx.delete(members).execute()
+			const p = await tx.delete(properties).execute()
+			const e = await tx.delete(entities).execute()
+			const s = await tx.delete(spaces).execute()
+			const c = await tx.delete(meta).execute()
 
-			return { v, r, ed, m, p, e, s, c };
-		});
+			return {v, r, ed, m, p, e, s, c}
+		})
 
-		return result;
-	});
+		return result
+	})
 
-	console.log("Transaction complete. Results:", results);
-}).pipe(Effect.provideServiceEffect(Storage, make));
+	console.log("Transaction complete. Results:", results)
+}).pipe(Effect.provideServiceEffect(Storage, make))
 
-Effect.runPromise(
-	reset.pipe(Effect.provideServiceEffect(Environment, makeEnvironment)),
-);
+Effect.runPromise(reset.pipe(Effect.provideServiceEffect(Environment, makeEnvironment)))
