47713abaa67534b8da942f2968f287d9b78804ff
Byron Guina <byron.guina@gmail.com>
2025-08-14 21:43:59 -0400

    fix: use unix timestamp for timestamp

--- stream/src/utils.rs
+++ stream/src/utils.rs
@@ -1,5 +1,4 @@
 use crate::pb::sf::substreams::rpc::v2::BlockScopedData;
-use chrono::DateTime;
 
 pub fn output(block_data: &BlockScopedData) -> &prost_types::Any {
     return block_data
@@ -21,15 +20,9 @@ pub struct BlockMetadata {
 pub fn block_metadata(block_data: &BlockScopedData) -> BlockMetadata {
     let clock = block_data.clock.as_ref().unwrap();
     let timestamp = clock.timestamp.as_ref().unwrap();
-    let date = DateTime::from_timestamp(timestamp.seconds, timestamp.nanos as u32)
-        .expect("received timestamp should always be valid");
 
     return BlockMetadata {
-        timestamp: (date
-            .signed_duration_since(chrono::offset::Utc::now())
-            .num_seconds()
-            * -1)
-            .to_string(),
+        timestamp: timestamp.seconds.to_string(),
         block_number: clock.number,
         cursor: block_data.cursor.clone(),
     };
