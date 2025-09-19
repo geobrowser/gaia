4c9f148f6250f36716691eb97c07978b0590fbc8
Byron Guina <byron.guina@gmail.com>
2025-09-19 16:23:41 -0700

    feat: new start block

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -16,7 +16,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 const PKG_FILE: &str = "geo_substream.spkg";
 const MODULE_NAME: &str = "geo_out";
-const START_BLOCK: i64 = 66877;
+const START_BLOCK: i64 = 67162;
 
 struct KgIndexer {
     storage: Arc<PostgresStorage>,
