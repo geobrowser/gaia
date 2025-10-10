b1cfaf98ac08b92cc0e872cc9ab3bd72b630c847
Byron Guina <byron.guina@gmail.com>
2025-10-10 13:23:42 -0700

    test: space id deriving

--- indexer_utils/src/id.rs
+++ indexer_utils/src/id.rs
@@ -44,6 +44,115 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_derive_space_id_deterministic() {
+        let network = "mainnet";
+        let dao_address = "0x1234567890123456789012345678901234567890";
+
+        let id1 = derive_space_id(network, dao_address);
+        let id2 = derive_space_id(network, dao_address);
+
+        assert_eq!(id1, id2, "Same inputs should produce same UUID");
+    }
+
+    #[test]
+    fn test_derive_space_id_different_networks() {
+        let dao_address = "0x1234567890123456789012345678901234567890";
+
+        let id1 = derive_space_id("mainnet", dao_address);
+        let id2 = derive_space_id("testnet", dao_address);
+
+        assert_ne!(id1, id2, "Different networks should produce different UUIDs");
+    }
+
+    #[test]
+    fn test_derive_space_id_different_dao_addresses() {
+        let network = "mainnet";
+
+        let id1 = derive_space_id(network, "0x1234567890123456789012345678901234567890");
+        let id2 = derive_space_id(network, "0x1234567890123456789012345678901234567891");
+
+        assert_ne!(id1, id2, "Different DAO addresses should produce different UUIDs");
+    }
+
+    #[test]
+    fn test_derive_space_id_address_normalization() {
+        let network = "mainnet";
+        
+        // Test that checksum_address normalization works
+        let id1 = derive_space_id(network, "0x1234567890123456789012345678901234567890");
+        let id2 = derive_space_id(network, "0x1234567890123456789012345678901234567890");
+
+        assert_eq!(id1, id2, "Same addresses should produce same UUID regardless of case");
+    }
+
+    #[test]
+    fn test_derive_space_id_edge_cases() {
+        // Test with empty network
+        let id1 = derive_space_id("", "0x1234567890123456789012345678901234567890");
+
+        // Test with short network name
+        let id2 = derive_space_id("a", "0x1234567890123456789012345678901234567890");
+
+        // Test with very long network name
+        let long_network = "very_long_network_name_that_might_cause_issues_if_not_handled_properly";
+        let id3 = derive_space_id(long_network, "0x1234567890123456789012345678901234567890");
+
+        // All should be different
+        assert_ne!(id1, id2, "Empty network and short network should produce different UUIDs");
+        assert_ne!(id1, id3, "Empty network and long network should produce different UUIDs");
+        assert_ne!(id2, id3, "Short network and long network should produce different UUIDs");
+    }
+
+    #[test]
+    fn test_derive_space_id_collision_resistance() {
+        // Test that similar but different inputs produce different results
+        let network1 = "mainnet";
+        let network2 = "testnet";
+        let dao_address = "0x1234567890123456789012345678901234567890";
+
+        let id1 = derive_space_id(network1, dao_address);
+        let id2 = derive_space_id(network2, dao_address);
+
+        assert_ne!(id1, id2, "Different networks should produce different UUIDs");
+
+        // Test with concatenated inputs that could potentially collide
+        let id3 = derive_space_id("main", "0x1234567890123456789012345678901234567890");
+        let id4 = derive_space_id("mainnet", "0x1234567890123456789012345678901234567890");
+
+        assert_ne!(id3, id4, "Different network lengths should not collide");
+    }
+
+    #[test]
+    fn test_derive_space_id_known_output() {
+        // Test with known inputs to ensure consistent output
+        let network = "ethereum";
+        let dao_address = "0xabcdefabcdefabcdefabcdefabcdefabcdefabcd";
+
+        let id = derive_space_id(network, dao_address);
+
+        // The UUID should be valid
+        assert_ne!(id, Uuid::nil(), "Generated UUID should not be nil");
+        
+        // Test consistency - same inputs should always produce this exact UUID
+        let id_again = derive_space_id(network, dao_address);
+        assert_eq!(id, id_again, "Should produce identical UUID for identical inputs");
+    }
+
+    #[test]
+    fn test_derive_space_id_vs_proposal_id_different() {
+        // Ensure space and proposal ID generation produce different results even with similar inputs
+        let dao_address = "0x1234567890123456789012345678901234567890";
+        let network = "mainnet";
+        let proposal_id = "mainnet"; // Using network name as proposal ID
+        let plugin_address = dao_address; // Using same address as plugin
+
+        let space_id = derive_space_id(network, dao_address);
+        let proposal_uuid = derive_proposal_id(dao_address, proposal_id, plugin_address);
+
+        assert_ne!(space_id, proposal_uuid, "Space ID and proposal ID should be different even with similar inputs");
+    }
+
+    #[test]
     fn test_derive_proposal_id_deterministic() {
         let dao_address = "0x1234567890123456789012345678901234567890";
         let proposal_id = "123";
