8f349ccaeb66908701acd6ec036c9e99ad4f1e39
0xjagger <194556652+0xJagger@users.noreply.github.com>
2026-05-01 12:12:27 -0300

    fix: proposal diff query

--- api/src/versioned/proposal-diff.ts
+++ api/src/versioned/proposal-diff.ts
@@ -135,12 +135,13 @@ function getProposalWithPublishAction(
 				SELECT
 					p.id as proposal_id,
 					p.space_id,
-					p.start_time,
-					p.end_time,
+					pv.start_time,
+					pv.end_time,
 					p.executed_at,
 					pa.content_uri
 				FROM proposals p
-				LEFT JOIN proposal_actions pa ON pa.proposal_id = p.id AND pa.action_type = 'Publish'
+				LEFT JOIN proposal_actions pa ON pa.proposal_id = p.id AND pa.action_type = 'Publish' and pa.proposal_version = p.current_version
+				LEFT JOIN proposal_version pv ON pv.proposal_id = p.id AND pv.proposal_version = p.current_version
 				WHERE p.id = ${proposalId}
 				LIMIT 1
 			`)
